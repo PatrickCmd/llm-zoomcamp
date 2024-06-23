3681a6c3754ee3632c533b18bbbd7ddda5dce44e
Walukagga Patrick <pwalukagga@gmail.com>
2024-06-23 17:27:29 +0000

    LLM introduction and environment setup

---
+++ bin/prepare.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+chmod +x bin/setup_dev_env.sh
+
+./bin/setup_dev_env.sh
---
+++ bin/setup_dev_env.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Move to the specified folder
+cd "$CODESPACE_VSCODE_FOLDER"
+
+# Download the Anaconda installer
+wget https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-x86_64.sh
+
+# Install Anaconda
+bash Anaconda3-2024.02-1-Linux-x86_64.sh -y
+
+# Initialize Conda for the current shell session
+source ~/anaconda3/bin/activate
+
+# Install necessary Python packages
+pip install tqdm openai elasticsearch groq
+
+# Confirm successful setup
+echo "Development environment setup complete!"

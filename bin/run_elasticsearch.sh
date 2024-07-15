73005b6929189e6e3e15dbe832f363b56b6339b5
Patrick Walukagga <pwalukagga@gmail.com>
2024-07-15 08:25:13 +0300

    Generate ground truth dataset

--- bin/run_elasticsearch.sh
+++ bin/run_elasticsearch.sh
@@ -2,12 +2,12 @@
 
 set -ex
 
-docker run -it \
+docker run --detach\
     --rm \
     --name elasticsearch \
+    -m 4GB \
     -p 9200:9200 \
     -p 9300:9300 \
     -e "discovery.type=single-node" \
     -e "xpack.security.enabled=false" \
-    -d \
     docker.elastic.co/elasticsearch/elasticsearch:8.4.3
